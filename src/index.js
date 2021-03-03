258d65ca661d292e428c7f15fcebf69bbb456ecd
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-03 22:00:23 +0300

    init GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/app';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
 ReactDOM.render(
-    <App />,
+    <>
+      <GlobalStyle/>
+      <App />
+    </>
+    ,
   document.getElementById('root')
 );

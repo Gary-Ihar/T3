b44db8d2ac5a945cda8c24e2d42e98570a6cac41
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-03 21:42:04 +0300

    add theme colors

---
+++ src/assets/theme/color.js
@@ -0,0 +1,7 @@
+export const color = {
+    helper_text: '#898989',
+    header_text: '#1B1B1B',
+    white: '#FFFFFF',
+    border_product_card_color: '#E6E6E6',
+    border_button_color: '#DBDBDB',
+}

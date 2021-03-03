497cacbb5b7bd14c1aaede053aece93cbee0ce18
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-03 20:47:45 +0300

    add Montserrat fonts

--- src/views/app.js
+++ src/views/app.js
@@ -1,5 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import "../assets/css/index.css";
+// TODO: До сих пор нет норм способа подключить шрифты в приложение с styled-components.
+// Сами авторы просят использовать font-display: fallback https://styled-components.com/docs/faqs#how-do-i-fix-flickering-text-after-server-side-rendering
+// Но даже на моей машине при достаточно раздутом проекте можно заметить это мерцание/блики. В нашей приложухе такого не будет. Нет роутинга и много чего еще.
+//
+// Я не считаю зазорным подрубить старый добрый CSS :)
 
 const Container = styled.div`
   margin: 0 auto;

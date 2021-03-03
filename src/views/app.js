6dc7b31724fb9af52f315e8b850d4265ccda5a2d
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-03 20:23:14 +0300

    add Container and Container styles(basic)

--- src/views/app.js
+++ src/views/app.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1300px;
+  display: grid;
+  grid-template-columns: 225px 1fr ;
+  grid-column-gap: 40px;
+`;
 
 const App = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <Container>
+    </Container>
+  );
+};
 
-export default App
+export default App;

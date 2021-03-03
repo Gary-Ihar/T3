7976e6ab74571aebe37b25b6f4e167ddb1af23cd
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-03 23:05:58 +0300

    add Image component(basic)

---
+++ src/components/image.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ImgStyle = styled.img`
+    object-fit: cover;
+    width: 100%;
+    max-height: ${({height})=>height}px;
+`
+
+const Image = ({imgName, alt, height}) => {
+    return (
+        <ImgStyle src={require(`../assets/img/${imgName}.png`).default} alt={alt || 'image'} height={height}/>
+    )
+}
+
+export default Image

ebd69484d97a6be999189f5d3738fe9eb69b2d20
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-04 10:49:25 +0300

    update Text styled component

--- src/assets/theme/styled-components.js
+++ src/assets/theme/styled-components.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-
+import { color as themeColor } from './color'
 export const Text = styled.p`
     font-family: Montserrat;
-    color: ${({color})=>color};
+    color: ${({color})=>color || themeColor.main_text};
     font-weight: ${({bold})=> bold ? 700 : 500};
     font-size: ${({size})=>size};
 `

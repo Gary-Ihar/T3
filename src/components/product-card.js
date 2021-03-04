344efa86b35113e293db5e728e94ad182acf0e55
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-04 11:14:01 +0300

    add style for Product Card(done)

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -10,15 +10,20 @@ const CardStyle = styled.div`
     display: flex;
     flex-direction: column;
     min-width: ${({width})=>width}px;
-    min-height: 401px;
     overflow: hidden;
     border: 1px solid ${({bColor})=>bColor};
     border-radius: 4px;
+    .title {
+        height: 40px;
+        overflow: hidden; // TODO: Текст лучше наверное скриптом обрезать, а не вот так вот.
+        margin-bottom: 11px;
+    }
     .price-section {
         width: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center;
+        margin-bottom: 18px;
     }
 `
 const Content = styled.div`
@@ -26,22 +31,36 @@ const Content = styled.div`
 `
 const IsNew = styled.div`
     position: absolute;
-    top: 10px;
-    left: 10px;
+    top: 12px;
+    left: 12px;
+    background-color: ${({BGColor})=>BGColor || color.new_product_bg_color};
+    padding: 6px 10px;
+    border-radius: 26px;
+`
+
+const PayButton = styled.button`
+    background-color: #FFFFFF;
+    border-radius: 8px;
+    padding: 10px 15px;
+    border: 1px solid ${({borderColor})=>borderColor};
+    cursor: pointer;
+    outline: none;
 `
 
 const ProductCard = ({data}) => {
+    console.log(data);
     return (
         <CardStyle width={productCardWidth} bColor={color.border_product_card_color}>
-            {data.is_new && <IsNew>новинка</IsNew>}
+            {data.is_new && <IsNew><Text size='10px' color={color.white} bold>новинка</Text></IsNew>}
             <Image imgName='foto' height='242'/>
             <Content>
-                <Text size="14px">{data.title}</Text>
+                <Text size="14px" className="title">{data.title}</Text>
                 <div className='price-section'>
-                    <Text size="16px" bold>{data.price} ₽</Text>
+                    <Text size="16px" bold>{data.price.slice(0, -3)} ₽</Text>{/* TODO: У меня не остается времени поставить пробел между цифрами. В этом ниче сложного, я на словах могу накидать как это сделать.
+                        Я бы завел хелпер функцию и через нее пропускал бы занчение helper(data.price)*/}
                     {data.is_second_hand && <Text size='11px'>Новое</Text>}
                 </div>
-                <button>В корзину</button>
+                <PayButton borderColor={color.border_button_color}>В корзину</PayButton>
             </Content>
         </CardStyle>
     )

92b7875aa55f67a81f800df009f883bf5959b622
Gary-Ihar <yakuzawot6@gmail.com>
2021-03-04 10:50:36 +0300

    add basic layout for Product Card

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 import { productCardWidth } from '../views/products'
 import Image from './image'
 import { color } from '../assets/theme/color'
+import { Text } from '../assets/theme/styled-components'
 
 const CardStyle = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     min-width: ${({width})=>width}px;
@@ -12,12 +14,35 @@ const CardStyle = styled.div`
     overflow: hidden;
     border: 1px solid ${({bColor})=>bColor};
     border-radius: 4px;
+    .price-section {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+`
+const Content = styled.div`
+    padding: 12px 16px 16px;
+`
+const IsNew = styled.div`
+    position: absolute;
+    top: 10px;
+    left: 10px;
 `
 
 const ProductCard = ({data}) => {
     return (
         <CardStyle width={productCardWidth} bColor={color.border_product_card_color}>
+            {data.is_new && <IsNew>новинка</IsNew>}
             <Image imgName='foto' height='242'/>
+            <Content>
+                <Text size="14px">{data.title}</Text>
+                <div className='price-section'>
+                    <Text size="16px" bold>{data.price} ₽</Text>
+                    {data.is_second_hand && <Text size='11px'>Новое</Text>}
+                </div>
+                <button>В корзину</button>
+            </Content>
         </CardStyle>
     )
 }
